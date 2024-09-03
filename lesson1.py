3019d73ea6ea987998b5d58fb8324a4694385679
Chaidez <machaidez3@cps.k12.il.us>
2024-09-03 12:28:46 -0500

    first commit

---
+++ lesson1.py
@@ -0,0 +1,26 @@
+# this is the print statement 
+#print statement allows us to print out a statement
+#comments are not executed by the computer
+#comments are used to help us understand the code 
+# print("hello my name is Mikey")
+# print("I am a student at hancock")
+# print("I am learning python")
+#create input for the user 
+name = input("what is your name?")
+print("Hello", name)
+#input allows us to take input from the user 
+
+#ask the user for their age 
+age = input("How old are you")
+#print out the age 
+print("You are", age, "years old")
+#ask the user for their favorite color
+color = input("What is your favorite color?")
+print("it is", color, "that's my favorite")
+#print out the favorite color Mikey
+#ask the user for their favorite food 
+food = input("What is your favorite food?")
+#print out their favorite food
+print ("Your favorite food is", food, "that is yummy")
+#concentration is the process of combining strings with 
+#variables 
